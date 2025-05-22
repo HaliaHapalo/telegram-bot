0928122165087316848be04439e6135b79051102
HaliaHapalo <166051148+HaliaHapalo@users.noreply.github.com>
2025-05-22 11:23:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+BOT_TOKEN = "1805866272:AAEVjVuESub14bCqG_BgkgLjWD1lscLdn6A"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привіт! Я твій бот.")
